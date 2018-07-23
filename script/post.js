c5467c350e891e6055fc296610cf87af83e2ae10
Yuming Meng <mengyuming@skycaster.net>
2018-07-23 11:44:47 +0800

    Site updated: 2018-07-23 11:44:46

---
+++ script/post.js
@@ -0,0 +1,3 @@
+// build time:Mon Jul 23 2018 11:02:41 GMT+0800 (中国标准时间)
+(function(d){var i=d("#header");if(window.innerWidth<480&&!i.hasClass("slide-hide")){i.addClass("slide-hide")}})(jQuery);
+//rebuild by neat 
