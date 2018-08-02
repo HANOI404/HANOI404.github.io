aabb2c1cda8f26afc0e9fc783297952528379215
Yuming Meng <mengyuming@skycaster.net>
2018-08-02 16:31:57 +0800

    Site updated: 2018-08-02 16:31:55

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Tue Jul 24 2018 09:42:01 GMT+0800 (中国标准时间)
+// build time:Thu Aug 02 2018 16:27:39 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
